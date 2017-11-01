9c4fa8141323a1bfbbacaf239e1336575de4631e
Akiyuki Okayasu <akiyuki@tkrworks.net>
2017-11-01 17:57:16 +0900

    v0.3.0

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -41,7 +41,7 @@
 namespace ProjectInfo
 {
     const char* const  projectName    = "JUCE-EssentiaMelodyDetection";
-    const char* const  versionString  = "0.2.1";
-    const int          versionNumber  = 0x201;
+    const char* const  versionString  = "0.3.0";
+    const int          versionNumber  = 0x300;
 }
 #endif
