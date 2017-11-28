9181aecca302a6b0165f78f7a29f519915308ce6
Akiyuki Okayasu <akiyuki@tkrworks.net>
2017-11-28 16:33:23 +0900

    v0.5.1

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -40,7 +40,7 @@
 namespace ProjectInfo
 {
     const char* const  projectName    = "MelodyEstimator";
-    const char* const  versionString  = "0.5.0";
-    const int          versionNumber  = 0x500;
+    const char* const  versionString  = "0.5.1";
+    const int          versionNumber  = 0x501;
 }
 #endif
